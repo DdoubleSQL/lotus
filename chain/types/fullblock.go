5475be3fbda65ad50587cff20483e10a9c1c4ccb
DdoubleSQL-github <ddouble2014@icloud.com>
2020-03-10 20:43:42 +0800

    ?

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -2,7 +2,9 @@ package types
 
 import "github.com/ipfs/go-cid"
 
+// 完整区块定义
 type FullBlock struct {
+	// 区块头
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
